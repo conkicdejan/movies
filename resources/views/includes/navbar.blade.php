72ed2b40824addb31df4d54166c538c37391ab98
Dejan Conkic <conkicdejan@gmail.com>
2022-05-24 18:47:38 +0200

    Task no.4

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,6 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="navbar-nav">
+        <a class="nav-item nav-link" href="/movies">Movies</a>
+        <a class="nav-item nav-link" href="/movies/create">Add movies</a>
+    </div>
+</nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     </head>
     <body class="antialiased">
+        @include('includes.navbar')
         @yield('content')
     </body>
 </html>
